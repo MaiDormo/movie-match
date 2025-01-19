523b099a5a8fe845323199ad0dc17c68bbc1b6df
maidormo <elia.gatti01@gmail.com>
2025-01-19 20:47:51 +0100

    remove duckduckgo adapter and interface_prova files and modified docker-compose

--- adapter_services/duckduckgo_adapter/routes/duckduckgo_routes.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-from controllers.duckduckgo_controller import health_check, perform_image_search
-
-router = APIRouter()
-
-router.get("/", response_model=dict)(health_check)
-router.get("/api/v1/get_image", response_model=dict)(perform_image_search)
